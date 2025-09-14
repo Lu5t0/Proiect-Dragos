4a15f5f6232223f2c96f6ebadd7546874179416e
Lu5t0 <sumerky2020@gmail.com>
2025-09-14 13:08:06 +0100

    Auth

--- main.py
+++ main.py
@@ -2,13 +2,9 @@
 # uvicorn main:app --host 0.0.0.0 --port 8000
 
 from fastapi import FastAPI
-from routers import login_router
-from routers import register_router
 from routers import auth_router
 
 
 app = FastAPI()
-app.include_router(register_router)
-app.include_router(login_router)
 app.include_router(auth_router)
 
--- routers/auth.py
+++ routers/auth.py
@@ -3,6 +3,12 @@ from pydantic import BaseModel,EmailStr
 import json
 
 router = APIRouter(prefix="/user", tags=["User"])
+User_FILE = "db/users.json"
+
+class User(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
 
 class AuthData(BaseModel):
     email: EmailStr
@@ -14,6 +20,36 @@ def read_users():
             return json.load(f)
     except FileNotFoundError:
         return []
+
+def write_users(users_list):
+    with open(User_FILE,"w") as f:
+        json.dump(users_list,f)
+
+@router.post("/register")
+def register_user(user : User):
+    user_list = read_users()
+    for u in user_list:
+        if u.get("email", "").lower() == user.email.lower():
+            raise HTTPException(status_code=400, detail="Email already registered")
+
+    new_user = {
+        "username": user.username,
+        "email": user.email,
+        "password": user.password
+    }
+    user_list.append(new_user)
+    write_users(user_list)
+    return {"message": "User registered successfully"}
+
+@router.post("/login")
+def login_user(credentials: AuthData):
+    user_list = read_users()
+    for u in user_list:
+        if u.get("email").lower() == credentials.email.lower() and u.get("password").lower() == credentials.password.lower():
+            logged_in_user = u
+            return {"message": f"Login Successful for {logged_in_user["email"]}"}
+
+    raise HTTPException(status_code=401, detail="Invalid credentials")
 
 def authenticate_user(auth: AuthData = Depends()):
     users = read_users()
