87384d5741203eb063e9bed078077ca72b35f71f
Lu5t0 <sumerky2020@gmail.com>
2025-09-15 19:35:58 +0100

    Auth V3

--- routers/cars.py
+++ routers/cars.py
@@ -7,13 +7,9 @@ from pydantic import BaseModel
 router = APIRouter(prefix="/cars", tags=["Cars"])
 cars_list = "db/cars.csv"
 
-class LoginRequest(BaseModel):
-    email: str
-    password: str
-
-@router.post("")
-def get_all_cars(data: LoginRequest):
-    user = authenticate_user(data.email, data.password)
+@router.get("")
+def get_all_cars(email: str, password: str):
+    user = authenticate_user(email,password)
     with open(cars_list, "r", newline="", encoding="utf-8") as csvfile:
         csv_dict_reader = csv.DictReader(csvfile)
         cars = list(csv_dict_reader)
@@ -22,8 +18,8 @@ def get_all_cars(data: LoginRequest):
         return cars
 
 @router.post("/add")
-def add_car(data: LoginRequest, manufacturer: str, model: str, year: str, transmission: str):
-    user = authenticate_user(data.email, data.password)
+def add_car(email: str, password: str, manufacturer: str, model: str, year: str, transmission: str):
+    user = authenticate_user(email, password)
     field_names = ["id", "manufacturer", "model", "year", "transmission"]
 
     with open(cars_list, "r+", newline="", encoding="utf-8") as csvfile:
